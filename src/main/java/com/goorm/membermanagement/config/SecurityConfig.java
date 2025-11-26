ef1d455673ef650a19c8c737936eac9a0177e00e
hyeonjune <annhj980@naver.com>
2025-11-27 01:11:12 +0900

    fix: Spring Security 설정 수정 403 오류 해결

--- src/main/java/com/goorm/membermanagement/config/SecurityConfig.java
+++ src/main/java/com/goorm/membermanagement/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.goorm.membermanagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -14,8 +14,8 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)  // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/member/login", "/member/register", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/member/**").authenticated()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/member/**").permitAll()
                         .anyRequest().denyAll()
                 )
                 .build();
